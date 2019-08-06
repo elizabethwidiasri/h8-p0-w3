ef7667054475c39fcb3c699a222d488255dba0b8
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-08-06 17:23:05 +0700

    done task15

--- exercise-15.js
+++ exercise-15.js
@@ -1,44 +1,31 @@
 /*
 Mengelompokkan Hewan
 
-Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya. Abjad harus berurut dari a-z (jika ada). Untuk kasus ini, anggap saja semua text lowercase.
-
+Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. 
+Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya.
+Abjad harus berurut dari a-z (jika ada). Untuk kasus ini, anggap saja semua text lowercase.
 Contoh jika animals adalah ["ayam", "kucing", "bebek", "bangau", "zebra"]
-
 maka akan menampilkan output: [ [ "ayam" ], [ "bebek", "bangau" ], [ "kucing ], [ "zebra" ] ]
-
 Urutan hewan dalam array setiap pengelompokan huruf tidak penting.
 */
 
-function groupAnimals(animals) {
-    // you can only write your code here!
-  }
-  
-  // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
-
-
-  
-
-  function groupAnimals(animals) {
-    var hasil = [];
+function groupAnimals(hewan) {
+    var group = [];
     var indeks = 0;
-    animals.sort();
-    for (var i = 0; i < animals.length; i++) {
-        if (hasil.length === 0) {
-            hasil.push([animals[i]]);
-        } else if (hasil[indeks][0][0] === animals[i][0]) {
-            hasil[indeks].push(animals[i]);
+    hewan.sort();
+    for (var i = 0; i < hewan.length; i++) {
+        if (group.length === 0) {
+            group.push([hewan[i]]);
+        } else if (group[indeks][0][0] === hewan[i][0]) {
+            group[indeks].push(hewan[i]);
         } else {
-            hasil.push([animals[i]]);
-            indeks++;
+            group.push([hewan[i]]);
+            indeks = indeks + 1;
         }
     }
-    return hasil;
+    return group;
 }
+
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
