f1fab7d49574d5344ac80d8b16f21a51f7dae75c
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-08-06 13:59:58 +0700

    done task9

--- exercise-9.js
+++ exercise-9.js
@@ -10,10 +10,15 @@ Kita perlu kemudian melakukan pembulatan angka dari hasil mean yang didapatkan (
 */
 
 function cariMean(arr) {
-    // you can only write your code here!
+
+  var arrSum = 0;
+   for(var i=0; i<arr.length; i++) {
+      arrSum += arr[i]
+   } 
+    return  Math.round(arrSum/arr.length)
   }
   
-  // TEST CASES
+  
   console.log(cariMean([1, 2, 3, 4, 5])); // 3
   console.log(cariMean([3, 5, 7, 5, 3])); // 5
   console.log(cariMean([6, 5, 4, 7, 3])); // 5
