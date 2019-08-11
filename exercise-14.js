f7c785ca98af600dcdcc8dba51c30f1554660055
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-08-11 17:23:14 +0700

    revise task14

--- exercise-14.js
+++ exercise-14.js
@@ -11,24 +11,22 @@ Function akan me-return array multidimensi dimana array tersebut berisikan 3 kat
 Contoh jika arr inputan adalah [45, 20, 21, 2, 7] maka output: [ [ 20, 2 ], [ 7 ], [ 45, 21 ] ]
 */
 
-
 function mengelompokkanAngka(arr) {
-    var arrayGroup = [[], [], []];
+    var hasil = [[], [], []]
     for (var i = 0; i < arr.length; i++) {
-        var angka = arr[i];
-        if (arr[i] % 2 === 0) {
-            arrayGroup[0].push(angka)
-        } else if (arr[i] % 2 !== 0) {
-            arrayGroup[1].push(angka);
-        } else if (arr[i] % 3 === 0) {
-            arrayGroup[2].push(angka)
+        if (arr[i] % 3 === 0) {
+            hasil[2].push(arr[i])
+        } else if (arr[i] % 2 === 0) {
+            hasil[0].push(arr[i])
+        } else {
+            hasil[1].push(arr[i])
         }
     }
-    return arrayGroup;
+    return hasil
 }
-
-// TEST CASES
-console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
-console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
-console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
-console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+  
+  // TEST CASES
+  console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+  console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+  console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+  console.log(mengelompokkanAngka([])); // [ [], [], [] ]
